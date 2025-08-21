cf8fdd61d34ec666f16eca5c71c334e68f7c65f4
Phloemus <brieucquemeneur@gmail.com>
2025-08-21 17:33:08 +0200

    build: adding the properties page

---
+++ frontend/src/pages/proprietes/index.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-xl font-bold">Mes propriétés</h1>
+        <div class="mt-4 flex flex-col md:flex-row">
+            <div class="my-2 p-1 rounded-sm cursor-pointer">
+                <div class="w-full h-44 bg-slate-300 rounded-sm"></div>
+                <div class="p-1">
+                    <div class="pb-2 flex justify-between">
+                        <h2 class="text-lg font-semibold">Appartement de Nantes</h2>
+                    </div>
+                    <div class="flex gap-2">
+                        <span class="px-2 py-0.5 text-sm bg-sky-200 text-sky-500 rounded-sm">Quittance</span>
+                        <span class="px-2 py-0.5 text-sm bg-green-200 text-green-500 rounded-sm">Rappels</span>
+                    </div>
+                </div>
+            </div>
+            <div class="my-2 p-1 rounded-sm cursor-pointer">
+                <div class="w-full h-44 bg-slate-300 rounded-sm"></div>
+                <div class="p-1">
+                    <div class="pb-2 flex justify-between">
+                        <h2 class="text-lg font-semibold">Appartement de Nantes</h2>
+                    </div>
+                    <div class="flex gap-2">
+                        <span class="px-2 py-0.5 text-sm bg-sky-200 text-sky-500 rounded-sm">Quittance</span>
+                        <span class="px-2 py-0.5 text-sm bg-green-200 text-green-500 rounded-sm">Rappels</span>
+                    </div>
+                </div>
+            </div>
+            <div class="my-2 p-1 rounded-sm cursor-pointer">
+                <div class="w-full h-44 bg-slate-300 rounded-sm"></div>
+                <div class="p-1">
+                    <div class="pb-2 flex justify-between">
+                        <h2 class="text-lg font-semibold">Appartement de Nantes</h2>
+                    </div>
+                    <div class="flex gap-2">
+                        <span class="px-2 py-0.5 text-sm bg-sky-200 text-sky-500 rounded-sm">Quittance</span>
+                        <span class="px-2 py-0.5 text-sm bg-green-200 text-green-500 rounded-sm">Rappels</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
