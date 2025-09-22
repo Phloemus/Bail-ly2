7ffe153f16d1b85d23e895b7540dd7e696d8fca2
Phloemus <brieucquemeneur@gmail.com>
2025-09-22 13:35:26 +0200

    build: adding more things to hte dashboard

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,48 +1,10 @@
 <script setup>
 
-    import { Index, Charset } from 'flexsearch'
-
-    import data from "../static/result-10k-first-classes.json"
-
-    const query = ref("")
-    const results = ref([])
-
-    const index = new Index({
-      encoder: Charset.LatinBalance,
-      tokenize: "forward", 
-      async: true, 
-    });
-
-    console.log(data.results.bindings.length)
-
-    data.results.bindings.forEach((data) => {
-        index.add(data.class.value, data.label.value)
-    })
-
-    function performSearch() {
-        if(query.value.length > 0) {
-            results.value = index.search(query.value)
-        } else {
-            results.value = []
-        }
-    }
-
 </script>
 
 <template>
-    <div>
-        <input v-model="query" @input="performSearch" placeholder="Search..." />
-        
-        <div v-if="results.length > 0">
-            <ul>
-                <li v-for="(result, index) in results" :key="index">
-                    {{ result }}
-                </li>
-            </ul>
-        </div>
-
-        <div v-else>
-            <p>No results found</p>
-        </div>
-  </div>
+    <main>
+	<div class="mx-auto p-4 lg:p-8 bg-white max-w-screen-xl border border-slate-200 rounded-md">
+	</div>
+    </main>
 </template>
